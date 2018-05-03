84d75fea74cc363ef4703925a2e2f0ae09593751
Romain DESCHAMPS <romain.deschamps@live.fr>
2018-05-03 18:10:23 +0200

    module Ouvrage : Correction sélection produits/service lors de l'ajout dans un ouvrage avec sélection ajax

--- htdocs/custom/ouvrage/js/ouvragecard.js
+++ htdocs/custom/ouvrage/js/ouvragecard.js
@@ -14,6 +14,27 @@ $(document).on("select2-selecting", '#productid, #serviceid', function (e) {
     setTimeout(function(){ $('#productid, #serviceid').val(0).trigger('change'); }, 20);
 });
 
+$(document).on("change", 'input#productid, input#serviceid', function (e) {
+	var id = $(this).val();
+	setTimeout(function(){
+		var text =  $('#search_'+e.currentTarget.id).val();
+
+		// Check si le produit est déjà dans l'ouvrage
+		if (id > 0 && $( "#sortable input[type=hidden][name='products[]'][value="+id+"]" ).length == 0) {
+			var newelt = $('<li class="ui-state-default"></li>');
+			$(newelt).append('<span class="icon-drag-drop"></span>');
+			$(newelt).append('<input type="hidden" name="products[]" value="'+id+'" />');
+			$(newelt).append(text);
+			$(newelt).append('<input type="number" name="quantity[]" value="1" length="4" />');
+			$(newelt).append('<a href="#" class="delete">X</a>');
+			$( "#sortable" ).append($(newelt));
+
+			setTimeout(function(){ $('#productid, #serviceid').val(0); }, 20);
+			setTimeout(function(){ $('#search_productid, #search_serviceid').val(''); }, 20);
+		}
+	}, 20);
+});
+
 $(document).on("click", '#sortable a.delete', function (e) {
     $(this).parent('li').remove();
 });
