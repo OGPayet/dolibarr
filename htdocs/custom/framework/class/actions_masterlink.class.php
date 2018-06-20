9b2031edcfafed7a16212d4778b1aa1679952dde
homer8173 <homer8173@gmail.com>
2018-06-20 23:45:55 +0200

    Fix loop on redirect

--- htdocs/custom/framework/class/actions_masterlink.class.php
+++ htdocs/custom/framework/class/actions_masterlink.class.php
@@ -15,64 +15,51 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+class ActionsMasterlink
+{
+
+    /**
+     * Overloading the doActions function : replacing the parent's function with the one below
+     *
+     * @param   array()         $parameters     Hook metadatas (context, etc...)
+     * @param   CommonObject    &$object        The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+     * @param   string          &$action        Current action (if set). Generally create or edit or null
+     * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+     * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+     */
+    function updateSession($parameters, &$object, &$action, $hookmanager)
+    {
+
+        global $conf, $db;
 
 
+        switch ($parameters['currentcontext']) {
+            case 'main':
 
-class ActionsMasterlink {
+                dol_include_once('/framework/class/masterlink.class.php');
+                $ml = new Masterlink($db);
 
+                $location = false;
+                if (isset($_SERVER['SCRIPT_URL']) && !empty($_SERVER['SCRIPT_URL'])) $url      = $_SERVER['SCRIPT_URL'];
+                else $url      = $_SERVER['PHP_SELF'];
 
-	/**
-	 * Overloading the doActions function : replacing the parent's function with the one below
-	 *
-	 * @param   array()         $parameters     Hook metadatas (context, etc...)
-	 * @param   CommonObject    &$object        The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
-	 * @param   string          &$action        Current action (if set). Generally create or edit or null
-	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
-	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
-	 */
-	function updateSession($parameters, &$object, &$action, $hookmanager)
-	{
+                $path     = substr($_SERVER['SCRIPT_URL'], strlen(DOL_URL_ROOT));
+                if ($path !== '' && $ml->fetch(0, $path) === 0 && $path != $ml->custom
+                ) $location = dol_buildPath($ml->custom, 2);
 
-		global $conf, $db;
+                if ($location) {
+                    $res = '';
+                    if (isset($_POST) && is_array($_POST)) foreach ($_POST as $k => $v)
+                            $res .= '&'.$k.'='.urlencode(GETPOST($k));
 
+                    header('location: '.$location.'?'.$_SERVER['QUERY_STRING'].$res);
+                    exit;
+                }
 
-		switch ($parameters['currentcontext']) {
-			case 'main':
-
-				dol_include_once('/framework/class/masterlink.class.php');
-				$ml = new Masterlink($db);
-
-				$location = false;
-				if(isset($_SERVER['SCRIPT_URL']) && !empty($_SERVER['SCRIPT_URL']) )
-					$url = $_SERVER['SCRIPT_URL'];
-				else
-					$url = $_SERVER['PHP_SELF'];
-
-				$path = substr($_SERVER['SCRIPT_URL'], strlen(DOL_URL_ROOT) );
-				if(
-							$ml->fetch(0, $path ) === 0
-							&& $path != $ml->custom
-					)
-					$location =  dol_buildPath($ml->custom, 2);
-
-			if($location){
-				$res = '';
-				if(isset($_POST) && is_array($_POST))
-					foreach($_POST as $k=>$v)
-						$res .= '&'.$k.'='.urlencode(GETPOST($k));
-
-				header('location: '.$location.'?'.$_SERVER['QUERY_STRING'].$res);
-				exit;
-			}
-
-			break;
-
-		}
+                break;
+        }
 
 
-			return 0; // or return 1 to replace standard code
-
-	}
-
-
+        return 0; // or return 1 to replace standard code
+    }
 }
