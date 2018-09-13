1c603cd91b40bb8983fb8330bb15e5f965d8d009
Julien Vercruysse <julien@elonet.fr>
2018-09-13 18:25:57 +0200

    Done - Ajout de l'onglet factures comprenant période et montant dans les fiches contrats - Correction du système de calcul des périodes - Correction de la gestion de la notion "d'échu ou à échoir" - Modification complète du système de génération de facture classique et de facture modèles (suite à la validation du modèle fonctionnel) - Correction de la gestion des proratas de début et de fin - Ajout des codes ajoutant les colonnes SQL des extrafields des contrats - Correction de la gestion des revalorisations - Blocage de facturation d'une période déjà facturé - Gestion des remises du tiers - Correction de l'affichage du première indice - Ajout de la gestion des facturations de contrat de période passé (50%) - Refonte du système de reconduction --
    TODO
    - Ajout du système de renouvellement
    - Ajout du système de résiliation
    - Ajout de la gestion des facturations de contrat de période passé (50%)
    - Ajout d'un événement sur le renouvellement ou la reconduction
    - Ajout d'une permission de résiliation
    - Ajout d'une variable permettant l'automatisation de la validation de la facture
    - Ajout d'un paramètre permettant le blocage de la facturation d'une période passé

--- htdocs/custom/synergiestechcontrat/core/modules/modsynergiestechcontrat.class.php
+++ htdocs/custom/synergiestechcontrat/core/modules/modsynergiestechcontrat.class.php
@@ -396,6 +396,40 @@ class modsynergiestechcontrat extends DolibarrModulessynergiestechcontrat
 			}
 		}
 
+		//Creation de la vue SQL
+		$sql_view = "CREATE VIEW `llx_view_c_indice` AS
+				SELECT
+						concat(`i`.`rowid`, '_Insee') AS `rowid`,
+						`i`.`year_indice` AS `year_indice`,
+						`i`.`month_indice` AS `month_indice`,
+						`i`.`indice` AS `indice`,
+					CONCAT(`i`.`year_indice`,'/',`i`.`month_indice`,' Insee') AS `label`,
+						'Insee' AS `filter`
+				FROM
+						`llx_c_indice_insee` `i`
+				WHERE
+						(`i`.`active` = 1)
+				UNION
+				SELECT
+						concat(`s`.`rowid`, '_Syntec') AS `rowid`,
+						`s`.`year_indice` AS `year_indice`,
+						`s`.`month_indice` AS `month_indice`,
+						`s`.`indice` AS `indice`,
+					CONCAT(`s`.`year_indice`,'/',`s`.`month_indice`,' Syntec') AS `label`,
+						'Syntec' AS `filter`
+				FROM
+						`llx_c_indice_syntec` `s`
+				WHERE
+						(`s`.`active` = 1);";
+
+		$resql=$db->query($sql_view);
+		if ($resql) {
+			$db->commit();
+		} else {
+			$db->rollback();
+		}
+
+
 		//ODT template
 		$src= dol_buildpath('/synergiestechcontrat/doctemplates/invoices/template_invoice.odt');
 		$dirodt=DOL_DATA_ROOT.'/doctemplates/invoices';
