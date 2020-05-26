1abc634a1737f77d9cb368941ca10bf2c47b3975
Alexis LAURIER <a.laurier@kreactive.com>
2020-05-26 19:08:19 +0200

    debug

--- htdocs/custom/synergiestech/core/dictionaries/activedirectorygroupmapping.dictionary.php
+++ htdocs/custom/synergiestech/core/dictionaries/activedirectorygroupmapping.dictionary.php
@@ -180,9 +180,9 @@ class ActiveDirectoryGroupMappingDictionary extends Dictionary
             $required_fields = array_unique(array_values(array_filter($required_fields, "dolValidElement")));
 
             if ($conf->global->LDAP_SERVER_TYPE == 'activedirectory') {
-                $ldapRecords = $ldap->getRecords('*', $conf->global->LDAP_GROUP_DN, $required_fields, "*", array($conf->global->LDAP_GROUP_FIELD_GROUPMEMBERS)); // Fiter on 'user' filter param
+                $ldapRecords = $ldap->getRecords('*', $conf->global->LDAP_GROUP_DN, $required_fields, "*");
             } else {
-                $ldapRecords = $ldap->getRecords('*', $conf->global->LDAP_GROUP_DN, $conf->global->LDAP_KEY_GROUPS, $required_fields, array($conf->global->LDAP_GROUP_FIELD_GROUPMEMBERS)); // Fiter on 'user' filter param
+                $ldapRecords = $ldap->getRecords('*', $conf->global->LDAP_GROUP_DN, $conf->global->LDAP_KEY_GROUPS, $required_fields);
             }
             print_r($ldap->error);
             print_r($ldapRecords);
