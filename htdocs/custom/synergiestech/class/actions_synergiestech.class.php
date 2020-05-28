2e54345e8ad287957bbf4d8a5e4ed2b9e3779414
Alexis LAURIER <a.laurier@kreactive.com>
2020-05-28 09:28:35 +0200

    hide billed column on supplier list

--- htdocs/custom/synergiestech/class/actions_synergiestech.class.php
+++ htdocs/custom/synergiestech/class/actions_synergiestech.class.php
@@ -3247,4 +3247,22 @@ SCRIPT;
 </script>
 SCRIPT;
     }*/
+
+    /**
+     * Overloading the printFieldPreListTitle function : replacing the parent's function with the one below
+     *
+     * @param   array()         $parameters     Hook metadatas (context, etc...)
+     * @param   CommonObject    &$object        The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+     * @param   string          &$action        Current action (if set). Generally create or edit or null
+     * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+     * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+     */
+    function printFieldPreListTitle(&$parameters, &$object, &$action, $hookmanager){
+        $contexts = explode(':', $parameters['context']);
+        if(in_array('supplierorderlist', $contexts)){
+            global $arrayfields;
+            $arrayfields['cf.billed']['checked'] = 0;
+            $arrayfields['cf.billed']['enabled'] = 0;
+        }
+    }
 }
