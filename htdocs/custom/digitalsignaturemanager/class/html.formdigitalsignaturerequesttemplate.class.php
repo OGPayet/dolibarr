bf46af502304e7d9990dc1b57b32a4f63a86d1e4
Alexis LAURIER <a.laurier@kreactive.com>
2020-12-17 23:22:15 +0100

    digital signature manager - correct hook answer

--- htdocs/custom/digitalsignaturemanager/class/html.formdigitalsignaturerequesttemplate.class.php
+++ htdocs/custom/digitalsignaturemanager/class/html.formdigitalsignaturerequesttemplate.class.php
@@ -211,7 +211,7 @@ class FormDigitalSignatureRequestTemplate
 		);
 		$parameters = array('question' => $questions);
 		$reshook = $hookmanager->executeHooks('addMoreFormQuestion', $parameters, $object, $action);
-		if (count($filesToBeDisplayed) == 1 && ($reshook < 0 || ($reshook == 0 && count($hookmanager->resArray) > 0))) {
+		if (count($filesToBeDisplayed) == 1 && ($reshook < 0 || ($reshook == 0 && count($hookmanager->resArray) == 0))) {
 			$action = self::CREATE_FROM_OBJECT_SIGNER_SELECTION_ACTION_NAME;
 			return $this->displayCreateFromSelectedFiles($object, $filesToBeDisplayed, $action, $hookmanager);
 		} else {
