d7a99f3e3cf343ad679bbafb052e55755b2a9796
Alexis LAURIER <a.laurier@kreactive.com>
2021-01-22 16:26:42 +0100

    update custom equipement societe tab for v13 compatibility

--- htdocs/custom/pagessubstitution/substitutions/1_synergies-tech/custom/equipement/tabs/societe.php
+++ htdocs/custom/pagessubstitution/substitutions/1_synergies-tech/custom/equipement/tabs/societe.php
@@ -184,7 +184,7 @@ if (DOL_VERSION >= "5.0.0") {
 }
 
 $sql = "SELECT";
-$sql .= " e.ref, e.rowid, e.fk_statut, e.fk_product, p.ref as refproduit, e.fk_entrepot, ent.label,";
+$sql .= " e.ref, e.rowid, e.fk_statut, e.fk_product, p.ref as refproduit, e.fk_entrepot, ent.ref,";
 $sql .= " e.fk_soc_fourn, sfou.nom as CompanyFourn, e.fk_facture_fourn, ff.ref as refFactureFourn,";
 $sql .= " e.fk_soc_client, scli.nom as CompanyClient, e.fk_facture, f.facnumber as refFacture,";
 $sql .= " e.datee, e.dateo, ee.libelle as etatequiplibelle";
@@ -204,7 +204,7 @@ if ($search_ref)			$sql .= " AND e.ref like '%" . $db->escape($search_ref) . "%'
 if ($search_refProduct)		$sql .= " AND p.ref like '%" . $db->escape($search_refProduct) . "%'";
 if ($search_company_fourn)	$sql .= " AND sfou.nom like '%" . $db->escape($search_company_fourn) . "%'";
 if ($search_reffact_fourn)	$sql .= " AND ff.ref like '%" . $db->escape($search_reffact_fourn) . "%'";
-if ($search_entrepot)		$sql .= " AND ent.label like '%" . $db->escape($search_entrepot) . "%'";
+if ($search_entrepot)		$sql .= " AND ent.ref like '%" . $db->escape($search_entrepot) . "%'";
 if ($search_company_client)	$sql .= " AND scli.nom like '%" . $db->escape($search_company_client) . "%'";
 if ($search_reffact_client)	$sql .= " AND f.facnumber like '%" . $db->escape($search_reffact_client) . "%'";
 if ($search_etatequipement)	$sql .= " AND e.fk_etatequipement =" . $search_etatequipement;
