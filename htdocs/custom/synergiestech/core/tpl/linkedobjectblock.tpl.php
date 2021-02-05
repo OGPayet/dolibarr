bc07e22475b9151d5d774fe478f39188a2a25c41
Alexis LAURIER <a.laurier@kreactive.com>
2021-02-05 10:00:59 +0100

    synergiestech module - hide price on propal linked objects when user no permission

--- htdocs/custom/synergiestech/core/tpl/linkedobjectblock.tpl.php
+++ htdocs/custom/synergiestech/core/tpl/linkedobjectblock.tpl.php
@@ -18,16 +18,15 @@
  */
 
 /**
- *  \file		htdocs/requestmanager/tpl/linkedobjectblock.tpl.php
- *  \ingroup	requestmanager
- *  \brief		Template to show objects linked to request manager
+ *  \file       htdocs/requestmanager/tpl/linkedobjectblock.tpl.php
+ *  \ingroup    requestmanager
+ *  \brief      Template to show objects linked to request manager
  */
 
 // Protection to avoid direct call of template
-if (empty($conf) || ! is_object($conf))
-{
-	print "Error, template page can't be called as URL";
-	exit;
+if (empty($conf) || ! is_object($conf)) {
+    print "Error, template page can't be called as URL";
+    exit;
 }
 
 ?>
@@ -60,7 +59,9 @@ if ($objecttype == 'equipement') {
         $objectlink->fetch_optionals();
 
         $trclass = ($var ? 'pair' : 'impair');
-        if ($ilink == count($linkedObjectBlock) && empty($noMoreLinkedObjectBlockAfter) && count($linkedObjectBlock) <= 1) $trclass .= ' liste_sub_total';
+        if ($ilink == count($linkedObjectBlock) && empty($noMoreLinkedObjectBlockAfter) && count($linkedObjectBlock) <= 1) {
+            $trclass .= ' liste_sub_total';
+        }
         ?>
       <tr class="<?php echo $trclass; ?>">
         <td><?php echo $langs->trans("Equipement"); ?></td>
@@ -71,7 +72,11 @@ if ($objecttype == 'equipement') {
         </td>
         <td><?php echo $productlink->label; ?></td>
         <td align="center"></td>
-		<td align="center"><?php if(!empty($objectlink->array_options['options_machineclient'])&&!empty($objectlink->array_options['options_idtelem'])) {echo $objectlink->array_options['options_idtelem'];} else {echo $productlink->getNomUrl(1);} ?></td>
+        <td align="center"><?php if (!empty($objectlink->array_options['options_machineclient'])&&!empty($objectlink->array_options['options_idtelem'])) {
+            echo $objectlink->array_options['options_idtelem'];
+} else {
+    echo $productlink->getNomUrl(1);
+} ?></td>
         <td align="right"><?php echo $objectlink->getLibStatut(3); ?></td>
         <td align="right"><a href="<?php echo $_SERVER["PHP_SELF"] . '?id=' . $object->id . '&action=dellink&dellinkid=' . $key; ?>"><?php echo img_delete($langs->transnoentitiesnoconv("RemoveLink")); ?></a>
         </td>
@@ -100,7 +105,9 @@ if ($objecttype == 'equipement') {
         $objectlink->fetch_optionals();
 
         $trclass = ($var ? 'pair' : 'impair');
-        if ($ilink == count($linkedObjectBlock) && empty($noMoreLinkedObjectBlockAfter) && count($linkedObjectBlock) <= 1) $trclass .= ' liste_sub_total';
+        if ($ilink == count($linkedObjectBlock) && empty($noMoreLinkedObjectBlockAfter) && count($linkedObjectBlock) <= 1) {
+            $trclass .= ' liste_sub_total';
+        }
         ?>
       <tr class="<?php echo $trclass; ?>">
         <td><?php echo $langs->trans("Contract"); ?></td>
@@ -129,7 +136,9 @@ if ($objecttype == 'equipement') {
         $ilink++;
 
         $trclass = ($var ? 'pair' : 'impair');
-        if ($ilink == count($linkedObjectBlock) && empty($noMoreLinkedObjectBlockAfter) && count($linkedObjectBlock) <= 1) $trclass .= ' liste_sub_total';
+        if ($ilink == count($linkedObjectBlock) && empty($noMoreLinkedObjectBlockAfter) && count($linkedObjectBlock) <= 1) {
+            $trclass .= ' liste_sub_total';
+        }
         ?>
       <tr class="<?php echo $trclass; ?>">
         <td><?php echo $langs->trans("Intervention"); ?></td>
@@ -144,8 +153,63 @@ if ($objecttype == 'equipement') {
       </tr>
         <?php
     }
+} elseif ($objecttype == 'propal') {
+    global $user, $conf;
+
+    $langs = $GLOBALS['langs'];
+    $linkedObjectBlock = $GLOBALS['linkedObjectBlock'];
+
+    // Load translation files required by the page
+    $langs->load("propal");
+
+    $linkedObjectBlock = dol_sort_array($linkedObjectBlock, 'date', 'desc', 0, 0, 1);
+
+    $total = 0;
+    $ilink = 0;
+    foreach ($linkedObjectBlock as $key => $objectlink) {
+        $ilink++;
+
+        $trclass = 'oddeven';
+        if ($ilink == count($linkedObjectBlock) && empty($noMoreLinkedObjectBlockAfter) && count($linkedObjectBlock) <= 1) {
+            $trclass .= ' liste_sub_total';
+        }
+        print '<tr class="'.$trclass.'"  data-element="'.$objectlink->element.'"  data-id="'.$objectlink->id.'" >';
+        print '<td class="linkedcol-element" >'.$langs->trans("Proposal");
+        if (!empty($showImportButton) && $conf->global->MAIN_ENABLE_IMPORT_LINKED_OBJECT_LINES) {
+            $url = DOL_URL_ROOT.'/comm/propal/card.php?id='.$objectlink->id;
+            print '<a class="objectlinked_importbtn" href="'.$url.'&amp;action=selectlines"  data-element="'.$objectlink->element.'"  data-id="'.$objectlink->id.'"  > <i class="fa fa-indent"></i> </a>';
+        }
+        print '</td>';
+        print '<td class="linkedcol-name nowraponall" >'.$objectlink->getNomUrl(1).'</td>';
+        print '<td class="linkedcol-ref" >'.$objectlink->ref_client.'</td>';
+        print '<td class="linkedcol-date center">'.dol_print_date($objectlink->date, 'day').'</td>';
+		print '<td class="linkedcol-amount right">';
+		 //-------------------------------------------------------------------------------
+        // Modification - Open-DSI - Begin
+        if ($user->rights->propale->lire && (!$conf->synergiestech->enabled || $user->rights->synergiestech->amount->customerpropal)) {
+		// Modification - Open-DSI - End
+        //-------------------------------------------------------------------------------
+			$total = $total + $objectlink->total_ht;
+            echo price($objectlink->total_ht);
+        }
+        print '</td>';
+        print '<td class="linkedcol-statut right">'.$objectlink->getLibStatut(3).'</td>';
+        print '<td class="linkedcol-action right"><a class="reposition" href="'.$_SERVER["PHP_SELF"].'?id='.$object->id.'&action=dellink&dellinkid='.$key.'">'.img_picto($langs->transnoentitiesnoconv("RemoveLink"), 'unlink').'</a></td>';
+        print "</tr>\n";
+    }
+    if (count($linkedObjectBlock) > 1) {
+        print '<tr class="liste_total '.(empty($noMoreLinkedObjectBlockAfter) ? 'liste_sub_total' : '').'">';
+        print '<td>'.$langs->trans("Total").'</td>';
+        print '<td></td>';
+        print '<td class="center"></td>';
+        print '<td class="center"></td>';
+        print '<td class="right">'.price($total).'</td>';
+        print '<td class="right"></td>';
+        print '<td class="right"></td>';
+        print "</tr>\n";
+    }
 } else {
-  return 0;
+    return 0;
 }
 ?>
 
