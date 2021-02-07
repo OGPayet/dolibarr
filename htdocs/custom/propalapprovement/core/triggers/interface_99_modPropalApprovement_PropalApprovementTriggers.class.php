62003548a6776d20c16cccf34a35139fa52a1a99
Alexis LAURIER <a.laurier@kreactive.com>
2021-02-07 18:51:31 +0100

    propal approvement - update copyrights

--- htdocs/custom/propalapprovement/core/triggers/interface_99_modPropalApprovement_PropalApprovementTriggers.class.php
+++ htdocs/custom/propalapprovement/core/triggers/interface_99_modPropalApprovement_PropalApprovementTriggers.class.php
@@ -1,5 +1,6 @@
 <?php
-/* Copyright (C) 2021 SuperAdmin <infra@synergies-france.fr>
+/* Copyright (C) 2021 Alexis LAURIER <contact@alexislaurier.fr>
+
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
