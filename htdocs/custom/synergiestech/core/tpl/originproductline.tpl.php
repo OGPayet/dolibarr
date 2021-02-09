d74e3c7b5bbf0eaa43484be87614e0239704d419
Alexis LAURIER <a.laurier@kreactive.com>
2021-02-09 08:51:08 +0100

    update synergiestech originline template for dolibarr v13

--- htdocs/custom/synergiestech/core/tpl/originproductline.tpl.php
+++ htdocs/custom/synergiestech/core/tpl/originproductline.tpl.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2010-2012	Regis Houssin	<regis.houssin@capnetworks.com>
+/* Copyright (C) 2010-2012  Regis Houssin   <regis.houssin@capnetworks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,10 +16,9 @@
  */
 
 // Protection to avoid direct call of template
-if (empty($conf) || ! is_object($conf))
-{
-	print "Error, template page can't be called as URL";
-	exit;
+if (empty($conf) || ! is_object($conf)) {
+    print "Error, template page can't be called as URL";
+    exit;
 }
 
 
@@ -45,15 +44,26 @@ if ($this->element == 'requestmanager' && ! empty($conf->contrat->enabled)) {
 
 <!-- BEGIN PHP TEMPLATE originproductline.tpl.php -->
 <tr <?php echo $bc[$var]; ?>>
-	<td><?php echo $this->tpl['label']; ?></td>
-	<td><?php echo $this->tpl['description']; ?></td>
-	<td align="right"><?php echo $this->tpl['vat_rate']; ?></td>
-	<td align="right"><?php echo $this->tpl['price']; ?></td>
-	<?php if (!empty($conf->multicurrency->enabled)) { ?><td align="right"><?php echo $this->tpl['multicurrency_price']; ?></td><?php } ?>
-	<td align="right"><?php echo $this->tpl['qty']; ?></td>
+    <td><?php echo $this->tpl['label']; ?></td>
+    <td><?php echo $this->tpl['description']; ?></td>
+    <td align="right"><?php echo $this->tpl['vat_rate']; ?></td>
+    <td align="right"><?php echo $this->tpl['price']; ?></td>
+    <?php if (!empty($conf->multicurrency->enabled)) {
+?><td align="right"><?php echo $this->tpl['multicurrency_price']; ?></td><?php
+} ?>
+    <td align="right"><?php echo $this->tpl['qty']; ?></td>
     <?php
-    if($conf->global->PRODUCT_USE_UNITS) echo '<td align="left">'.$langs->trans($this->tpl['unit']).'</td>';
+    if ($conf->global->PRODUCT_USE_UNITS) {
+        echo '<td align="left">'.$langs->trans($this->tpl['unit']).'</td>';
+    }
     ?>
-	<td align="right"><?php echo $this->tpl['remise_percent']; ?></td>
+    <td align="right"><?php echo $this->tpl['remise_percent']; ?></td>
+    <?php
+    $selected = 1;
+if (!empty($selectedLines) && !in_array($this->tpl['id'], $selectedLines)) $selected = 0;
+print '<td class="center">';
+print '<input id="cb'.$this->tpl['id'].'" class="flat checkforselect" type="checkbox" name="toselect[]" value="'.$this->tpl['id'].'"'.($selected ? ' checked="checked"' : '').'>';
+print '</td>';
+?>
 </tr>
 <!-- END PHP TEMPLATE originproductline.tpl.php -->
