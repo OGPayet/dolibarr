a7a175ce14a89098a006de8319f11297bfbd9203
Pierre Payet <pierre.p@synergies-france.fr>
2021-02-12 14:45:14 +0100

    fix langs in InterventionCheckFields class

--- htdocs/custom/interventionsurvey/lib/interventionsurvey_checkinterventionfields.lib.php
+++ htdocs/custom/interventionsurvey/lib/interventionsurvey_checkinterventionfields.lib.php
@@ -1,6 +1,7 @@
 <?php
 
 class InterventionCheckFields {
+    var $langs;
     var $object;
     var $errors;
 
@@ -10,18 +11,21 @@ class InterventionCheckFields {
 	 * @param DoliDB $db Database handler
 	*/
 	function __construct($object) {
+        global $langs;
+
+        $langs->load("interventionsurvey@interventionsurvey");
+
+        $this->langs = $langs;
         $this->object = $object;
 		$this->errors = array();
     }
 
     // Check array options
     public function isArrayOptionsEmpty() {
-        global $langs;
-
         $error = array();
 
         if (empty($this->object->array_options)) {
-            $error[] = $langs->trans('InterventionSurveyMissingArrayOptions', $this->object->id); 
+            $error[] = $this->langs->trans('InterventionSurveyMissingArrayOptions', $this->object->id); 
         }
 
         return $error;
@@ -29,14 +33,12 @@ class InterventionCheckFields {
 
     // Check stakeholder signature
     public function isStakeholderSignatureEmpty() {
-        global $langs;
-
         $error = array();
 
         if (empty($this->object->array_options['options_stakeholder_signature'])) {
-            $error[] = $langs->trans('InterventionSurveyMissingStakeholderSignature', $this->object->id); 
+            $error[] = $this->langs->trans('InterventionSurveyMissingStakeholderSignature', $this->object->id); 
         } else if (empty(json_decode($this->object->array_options['options_stakeholder_signature'])->value)) {
-            $error[] = $langs->trans('InterventionSurveyMissingStakeholderSignature', $this->object->id); 
+            $error[] = $this->langs->trans('InterventionSurveyMissingStakeholderSignature', $this->object->id); 
         }
 
         return $error;
@@ -44,15 +46,13 @@ class InterventionCheckFields {
 
     // Check customer signature
     public function isCustomerSignatureEmpty() {
-        global $langs;
-
         $error = array();
 
         if (empty($this->object->array_options['options_customer_signature'])) {
-            $error[] = $langs->trans('InterventionSurveyMissingCustomerSignature', $this->object->id); 
+            $error[] = $this->langs->trans('InterventionSurveyMissingCustomerSignature', $this->object->id); 
         } else if (empty(json_decode($this->object->array_options['options_customer_signature'])->value)) {
             if (!json_decode($this->object->array_options['options_customer_signature'])->isCustomerAbsent) {
-                $error[] = $langs->trans('InterventionSurveyMissingCustomerSignature', $this->object->id); 
+                $error[] = $this->langs->trans('InterventionSurveyMissingCustomerSignature', $this->object->id); 
             }
         }
 
@@ -61,12 +61,10 @@ class InterventionCheckFields {
 
     // Check intervention lines
     public function isInterventionLinesEmpty() {
-        global $langs;
-
         $error = array();
 
         if (empty($this->object->lines)) {
-            $error[] = $langs->trans('InterventionSurveyMissingInterventionLines', $this->object->label, $this->object->id); 
+            $error[] = $this->langs->trans('InterventionSurveyMissingInterventionLines', $this->object->label, $this->object->id); 
         }
 
         return $error;
