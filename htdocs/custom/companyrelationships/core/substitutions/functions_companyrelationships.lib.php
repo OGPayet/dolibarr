781bce3563b5d5a85cdc6d99fb326d87d7d6d4eb
Pierre Payet <pierre.p@synergies-france.fr>
2021-03-03 11:11:14 +0100

    add all substitutions var for benefactor & watcher

--- htdocs/custom/companyrelationships/core/substitutions/functions_companyrelationships.lib.php
+++ htdocs/custom/companyrelationships/core/substitutions/functions_companyrelationships.lib.php
@@ -54,16 +54,56 @@ function companyrelationships_completesubstitutionarray(&$substitutionarray, $la
     if ($object->array_options['options_companyrelationships_fk_soc_benefactor']) {
         $societe = new Societe($db);
         $societe->fetch($object->array_options['options_companyrelationships_fk_soc_benefactor']);
-        $benefactor_name = $societe->nom;
 
-        $substitutionarray['__BENEFACTOR_NAME__'] = $benefactor_name;
+        $substitutionarray['__BENEFACTOR_ID__'] = (is_object($societe) ? $societe->id : '');
+        $substitutionarray['__BENEFACTOR_NAME__'] = (is_object($societe) ? $societe->name : '');
+        $substitutionarray['__BENEFACTOR_NAME_ALIAS__'] = (is_object($societe) ? $societe->name_alias : '');
+        $substitutionarray['__BENEFACTOR_CODE_CLIENT__'] = (is_object($societe) ? $societe->code_client : '');
+        $substitutionarray['__BENEFACTOR_CODE_FOURNISSEUR__'] = (is_object($societe) ? $societe->code_fournisseur : '');
+        $substitutionarray['__BENEFACTOR_EMAIL__'] = (is_object($societe) ? $societe->email : '');
+        $substitutionarray['__BENEFACTOR_PHONE__'] = (is_object($societe) ? $societe->phone : '');
+        $substitutionarray['__BENEFACTOR_FAX__'] = (is_object($societe) ? $societe->fax : '');
+        $substitutionarray['__BENEFACTOR_ADDRESS__'] = (is_object($societe) ? $societe->address : '');
+        $substitutionarray['__BENEFACTOR_ZIP__'] = (is_object($societe) ? $societe->zip : '');
+        $substitutionarray['__BENEFACTOR_TOWN__'] = (is_object($societe) ? $societe->town : '');
+        $substitutionarray['__BENEFACTOR_COUNTRY_ID__'] = (is_object($societe) ? $societe->country_id : '');
+        $substitutionarray['__BENEFACTOR_COUNTRY_CODE__'] = (is_object($societe) ? $societe->country_code : '');
+        $substitutionarray['__BENEFACTOR_IDPROF1__'] = (is_object($societe) ? $societe->idprof1 : '');
+        $substitutionarray['__BENEFACTOR_IDPROF2__'] = (is_object($societe) ? $societe->idprof2 : '');
+        $substitutionarray['__BENEFACTOR_IDPROF3__'] = (is_object($societe) ? $societe->idprof3 : '');
+        $substitutionarray['__BENEFACTOR_IDPROF4__'] = (is_object($societe) ? $societe->idprof4 : '');
+        $substitutionarray['__BENEFACTOR_IDPROF5__'] = (is_object($societe) ? $societe->idprof5 : '');
+        $substitutionarray['__BENEFACTOR_IDPROF6__'] = (is_object($societe) ? $societe->idprof6 : '');
+        $substitutionarray['__BENEFACTOR_TVAINTRA__'] = (is_object($societe) ? $societe->tva_intra : '');
+        $substitutionarray['__BENEFACTOR_NOTE_PUBLIC__'] = (is_object($societe) ? dol_htmlentitiesbr($societe->note_public) : '');
+        $substitutionarray['__BENEFACTOR_NOTE_PRIVATE__'] = (is_object($societe) ? dol_htmlentitiesbr($societe->note_private) : '');
     }
 
     if ($object->array_options['options_companyrelationships_fk_soc_watcher']) {
         $societe = new Societe($db);
         $societe->fetch($object->array_options['options_companyrelationships_fk_soc_watcher']);
-        $watcher_name = $societe->nom;
 
-        $substitutionarray['__WATCHER_NAME__'] = $watcher_name;
+        $substitutionarray['__WATCHER_ID__'] = (is_object($societe) ? $societe->id : '');
+        $substitutionarray['__WATCHER_NAME__'] = (is_object($societe) ? $societe->name : '');
+        $substitutionarray['__WATCHER_NAME_ALIAS__'] = (is_object($societe) ? $societe->name_alias : '');
+        $substitutionarray['__WATCHER_CODE_CLIENT__'] = (is_object($societe) ? $societe->code_client : '');
+        $substitutionarray['__WATCHER_CODE_FOURNISSEUR__'] = (is_object($societe) ? $societe->code_fournisseur : '');
+        $substitutionarray['__WATCHER_EMAIL__'] = (is_object($societe) ? $societe->email : '');
+        $substitutionarray['__WATCHER_PHONE__'] = (is_object($societe) ? $societe->phone : '');
+        $substitutionarray['__WATCHER_FAX__'] = (is_object($societe) ? $societe->fax : '');
+        $substitutionarray['__WATCHER_ADDRESS__'] = (is_object($societe) ? $societe->address : '');
+        $substitutionarray['__WATCHER_ZIP__'] = (is_object($societe) ? $societe->zip : '');
+        $substitutionarray['__WATCHER_TOWN__'] = (is_object($societe) ? $societe->town : '');
+        $substitutionarray['__WATCHER_COUNTRY_ID__'] = (is_object($societe) ? $societe->country_id : '');
+        $substitutionarray['__WATCHER_COUNTRY_CODE__'] = (is_object($societe) ? $societe->country_code : '');
+        $substitutionarray['__WATCHER_IDPROF1__'] = (is_object($societe) ? $societe->idprof1 : '');
+        $substitutionarray['__WATCHER_IDPROF2__'] = (is_object($societe) ? $societe->idprof2 : '');
+        $substitutionarray['__WATCHER_IDPROF3__'] = (is_object($societe) ? $societe->idprof3 : '');
+        $substitutionarray['__WATCHER_IDPROF4__'] = (is_object($societe) ? $societe->idprof4 : '');
+        $substitutionarray['__WATCHER_IDPROF5__'] = (is_object($societe) ? $societe->idprof5 : '');
+        $substitutionarray['__WATCHER_IDPROF6__'] = (is_object($societe) ? $societe->idprof6 : '');
+        $substitutionarray['__WATCHER_TVAINTRA__'] = (is_object($societe) ? $societe->tva_intra : '');
+        $substitutionarray['__WATCHER_NOTE_PUBLIC__'] = (is_object($societe) ? dol_htmlentitiesbr($societe->note_public) : '');
+        $substitutionarray['__WATCHER_NOTE_PRIVATE__'] = (is_object($societe) ? dol_htmlentitiesbr($societe->note_private) : '');
     }
 }
