c959e200057c9eaeb18b135d72db3d22750a63b1
Alexis LAURIER <a.laurier@kreactive.com>
2021-04-12 17:37:56 +0200

    fix sall on invoice better status

--- htdocs/custom/invoicebetterstatus/class/actions_invoicebetterstatus.class.php
+++ htdocs/custom/invoicebetterstatus/class/actions_invoicebetterstatus.class.php
@@ -413,6 +413,26 @@ class ActionsInvoiceBetterStatus
         }
     }
 
+	/**
+     * Overloading the showLinkedObjectBlock function : replacing the parent's function with the one below
+     *
+     * @param   array           $parameters     Hook metadatas (context, etc...)
+     * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+     * @param   string          $action         Current action (if set). Generally create or edit or null
+     * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+     * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+     */
+	public function printFieldListFrom($parameters, &$object, &$action, $hookmanager)
+	{
+		$contexts = explode(':', $parameters['context']);
+        if (in_array('invoicelist', $contexts)) {
+			global $sall;
+			if($sall) {
+				$this->resprints = ' LEFT JOIN '.MAIN_DB_PREFIX.'paiement_facture as pf ON pf.fk_facture = f.rowid';
+			}
+        }
+	}
+
     /**
      * Function to get current selected searched status
      * @return  int[] $array of better status ids
